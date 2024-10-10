a3fd48dde9b0e26d156eba57905c1211de91c78a
Alvaro Souza <dan.alvarosouza@gmail.com>
2024-10-10 14:53:31 -0300

    Implementando as restrições de movimento

--- XadrezConsole/Tabuleiro/Peca.cs
+++ XadrezConsole/Tabuleiro/Peca.cs
@@ -7,7 +7,7 @@
         public int qteMovimento { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Tabuleiro tab, Cor cor )
+        public Peca(Tabuleiro tab, Cor cor)
         {
             Posicao = null;
             Cor = cor;
@@ -15,11 +15,33 @@
             this.tab = tab;
         }
 
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool podeMoverPara(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
+        }
         public abstract bool[,] movimentosPossiveis();
-                
+
         public void incrementarQteMovimentos()
         {
             qteMovimento++;
         }
+
+
     }
 }
