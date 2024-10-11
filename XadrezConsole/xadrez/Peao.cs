02b3ce85b25b13918aec437ed4b639698c0f737c
Alvaro Souza <dan.alvarosouza@gmail.com>
2024-10-11 12:18:40 -0300

    Jogada especial En Passant

--- XadrezConsole/xadrez/Peao.cs
+++ XadrezConsole/xadrez/Peao.cs
@@ -4,7 +4,11 @@ namespace xadrez
 {
     internal class Peao : Peca
     {
-        public Peao(Tabuleiro tab, Cor cor) : base(tab, cor) { }
+        private PartidaDeXadrez partida;
+        public Peao(Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(tab, cor)
+        {
+            this.partida = partida;
+        }
 
         public override string ToString()
         {
@@ -17,7 +21,7 @@ namespace xadrez
             return p != null && p.Cor != Cor;
         }
 
-        private bool livre(Posicao pos) 
+        private bool livre(Posicao pos)
         {
             return tab.peca(pos) == null;
         }
@@ -54,6 +58,23 @@ namespace xadrez
                     mat[pos.linha, pos.coluna] = true;
                 }
 
+                // Jogada especial En Passant peão branco
+                if (Posicao.linha == 3)
+                {
+                    Posicao esquerda = new Posicao(Posicao.linha, Posicao.coluna - 1);
+                    if (tab.posicaoValida(esquerda) && existeInimigo(esquerda) && tab.peca(esquerda) == partida.vulneravelEnPassant)
+                    {
+                        mat[esquerda.linha - 1, esquerda.coluna] = true;
+                    }
+
+                    Posicao direita = new Posicao(Posicao.linha, Posicao.coluna + 1);
+                    if (tab.posicaoValida(direita) && existeInimigo(direita) && tab.peca(direita) == partida.vulneravelEnPassant)
+                    {
+                        mat[direita.linha - 1, direita.coluna] = true;
+                    }
+
+                }
+
             }
             else
             {
@@ -79,6 +100,23 @@ namespace xadrez
                 if (tab.posicaoValida(pos) && existeInimigo(pos))
                 {
                     mat[pos.linha, pos.coluna] = true;
+                }
+
+                // Jogada especial En Passant peão preto
+                if (Posicao.linha == 4)
+                {
+                    Posicao esquerda = new Posicao(Posicao.linha, Posicao.coluna - 1);
+                    if (tab.posicaoValida(esquerda) && existeInimigo(esquerda) && tab.peca(esquerda) == partida.vulneravelEnPassant)
+                    {
+                        mat[esquerda.linha + 1, esquerda.coluna] = true;
+                    }
+
+                    Posicao direita = new Posicao(Posicao.linha, Posicao.coluna + 1);
+                    if (tab.posicaoValida(direita) && existeInimigo(direita) && tab.peca(direita) == partida.vulneravelEnPassant)
+                    {
+                        mat[direita.linha + 1, direita.coluna] = true;
+                    }
+
                 }
             }
             return mat;
